e9aa5fb3b520a17a34e8c17dd1e83fc9e6c80f88
Brad Hamill <brad@hamill.com>
2020-01-20 17:39:04 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const userRoutes = require('./routes/user');
+
+const app = express();
+
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology:true
+}).then(() => console.log('DB Connected'))
+
+app.use('/api', userRoutes);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
